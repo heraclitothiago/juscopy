5dcad11773159ff4a6546cf968b2177f830eca7c
Heraclito Thiago <heraclitothiago@hotmail.com>
2021-05-15 13:57:15 -0300

    refatorado o código, retorno ao status quo

--- copy.js
+++ copy.js
@@ -22,4 +22,17 @@ var queryElement = (query) => {
     return document.querySelector(query)
 }
 
-jsonValues.run
+(async openEmenta => {
+    await awaitToExec(5)
+    var openEmenta = queryElement(jsonValues.ementaSeletor)
+    openEmenta.click()
+    var jurisprudencia = queryElement(jsonValues.jurisprudenciaSelector)
+    const el = document.createElement('textarea')
+    el.value = jurisprudencia.innerText;
+    document.body.appendChild(el)
+    el.select();
+    document.execCommand('copy')
+    document.body.removeChild(el)
+    var donation = queryElement(jsonValues.donationSelector)
+    donation.innerHTML = `${jsonValues.successMessage} ${jsonValues.pngFile}`;
+})()
