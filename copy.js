ace9bd44a100496bb1ac1fa7b732178cfcd2d3d2
Thiago Castro <34630331+heraclitothiago@users.noreply.github.com>
2021-10-26 22:59:03 -0300

    Update copy.js

--- copy.js
+++ copy.js
@@ -5,20 +5,22 @@ var jurisContent = async () => {
     return textJuris = await jurisprudencia.innerText;
 }
 
-await jurisContent()
-var newContainer = document.createElement("textarea");
-newContainer.value = textJuris;
-document.body.appendChild(newContainer);
-newContainer.select();
-document.execCommand("copy");
-document.body.removeChild(newContainer);
-document.querySelector(".CopyContentModal-body").innerHTML = `
-    <h2>Jurisprudência copiada com sucesso!!!</h2>
-    <hr>
-    <p style="line-height: 20px;">Nosso script será atualizado em breve e teremos muitas novidades 🎉🥳</p>
-    <p style="line-height: 20px;">Faça uma contribuição de <b>qualquer valor</b> incentive o desenvolvimento de facilidades que o beneficiarão sempre 😊<br>
-    <b>Chave pix:<b> dradvloper@gmail.com</p>
-    <img src="https://github.com/heraclitothiago/juscopy/raw/main/assets/download.png" alt="pix" style="width: 180px;">
-    `
-console.log("%cA Jurisprudência foi copiada!", "background-color: orange; font-size: 15px;");
-document.querySelector('.CopyContentModal-copyButton').remove()
+(() => {
+    await jurisContent()
+    var newContainer = document.createElement("textarea");
+    newContainer.value = textJuris;
+    document.body.appendChild(newContainer);
+    newContainer.select();
+    document.execCommand("copy");
+    document.body.removeChild(newContainer);
+    document.querySelector(".CopyContentModal-body").innerHTML = `
+        <h2>Jurisprudência copiada com sucesso!!!</h2>
+        <hr>
+        <p style="line-height: 20px;">Nosso script será atualizado em breve e teremos muitas novidades 🎉🥳</p>
+        <p style="line-height: 20px;">Faça uma contribuição de <b>qualquer valor</b> incentive o desenvolvimento de facilidades que o beneficiarão sempre 😊<br>
+        <b>Chave pix:<b> dradvloper@gmail.com</p>
+        <img src="https://github.com/heraclitothiago/juscopy/raw/main/assets/download.png" alt="pix" style="width: 180px;">
+        `
+    console.log("%cA Jurisprudência foi copiada!", "background-color: orange; font-size: 15px;");
+    document.querySelector('.CopyContentModal-copyButton').remove()
+})()
